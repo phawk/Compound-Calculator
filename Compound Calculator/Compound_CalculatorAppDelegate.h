d8d63c3db163034c4fa59468e92930948634fb8f
Pete Hawkins <pete@phawk.co.uk>
2011-09-10 00:31:17 +0100

    Initial Commit

---
+++ Compound Calculator/Compound_CalculatorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Compound_CalculatorAppDelegate.h
+//  Compound Calculator
+//
+//  Created by Pete Hawkins on 10/09/2011.
+//  Copyright 2011 phawk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Compound_CalculatorAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Compound Calculator/Compound_CalculatorAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  Compound_CalculatorAppDelegate.m
+//  Compound Calculator
+//
+//  Created by Pete Hawkins on 10/09/2011.
+//  Copyright 2011 phawk. All rights reserved.
+//
+
+#import "Compound_CalculatorAppDelegate.h"
+
+@implementation Compound_CalculatorAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
